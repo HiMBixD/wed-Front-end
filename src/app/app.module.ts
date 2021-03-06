2a00a1e2d29ff5f6e3c592a4eed045bb097bce35
Uyen Nguyen <55873366+uyen18827@users.noreply.github.com>
2021-03-06 11:04:10 +0700

    dependency: added Bootstrap
    integrated npm version of bootstrap into the project. It's working now.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 @NgModule({
   declarations: [
