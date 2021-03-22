b5d2fa0a3f2f16f112279a499975251db88156a2
Uyen Nguyen <55873366+uyen18827@users.noreply.github.com>
2021-03-22 19:33:10 +0700

    func: add mock data to selected submission browser

--- src/app/auth-required-pages/containers/selected-submission-browser/selected-submission-browser.component.ts
+++ src/app/auth-required-pages/containers/selected-submission-browser/selected-submission-browser.component.ts
@@ -11,5 +11,33 @@ export class SelectedSubmissionBrowserComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  p : number = 1
 
+  mockSelected: selectedSubmission[] = [
+    {
+      topic: 'Tech trend',
+      username: 'mimi123',
+      date: '21/2/2021'
+    },
+    {
+      topic: 'Tech trend',
+      username: 'hihi999',
+      date: '21/2/2021'
+    },
+    {
+      topic: 'Future of AI',
+      username: 'ha2001',
+      date: '5/3/2021'
+    },
+    {
+      topic: 'Future of AI',
+      username: 'myEye2080',
+      date: '9/3/2021'
+    },
+  ]
+}
+interface selectedSubmission  {
+  topic: string,
+  username: string,
+  date: string,
 }
