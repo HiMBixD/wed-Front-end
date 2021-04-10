30678f09390c79448c7bf66ca85f6306262815ab
havorx <55874008+jazzkatxlemon@users.noreply.github.com>
2021-04-11 03:31:10 +0700

    done dashboard

--- src/app/auth-required-pages/containers/management-dashboard/management-dashboard.component.ts
+++ src/app/auth-required-pages/containers/management-dashboard/management-dashboard.component.ts
@@ -1,10 +1,11 @@
+/* tslint:disable:no-shadowed-variable */
 import {Component, OnInit} from '@angular/core';
 import {ChartOptions, ChartType} from 'chart.js';
 import {Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip, SingleDataSet} from 'ng2-charts';
 import {mockNoCommentYet} from '../../interfaces/exReport';
 import {CommonService} from '../../services/common.service';
-import {exReport} from '../../interfaces/exReport';
 import {assignment} from '../../interfaces/assignment';
+
 
 @Component({
   selector: 'app-management-dashboard',
@@ -13,11 +14,12 @@ import {assignment} from '../../interfaces/assignment';
 })
 export class ManagementDashboardComponent implements OnInit {
   private facultyName: any;
-  private selectedSubmissions: any;
   private assignmentList: any;
-  private submissionList: any;
-  private deadlineId: any;
+  public submissionList: any;
   private assignment: assignment;
+  public overDueSub = [];
+  public notCommentedYet = [];
+  private SubCount: any;
 
 
   constructor(private commonService: CommonService) {
@@ -25,11 +27,10 @@ export class ManagementDashboardComponent implements OnInit {
     monkeyPatchChartJsLegend();
   }
 
-  p = 1;
-  exReport: exReport;
-  noCommentYet = mockNoCommentYet;
-  facultyList: any;
-  currentYear = new Date().getFullYear();
+  public p = 1;
+  private facultyList = [];
+  private currentYear = new Date().getFullYear();
+
   public pieChartOptions: ChartOptions = {
     title: {
       text: `Total Contributions - ${this.currentYear}`,
@@ -40,50 +41,91 @@ export class ManagementDashboardComponent implements OnInit {
   };
 
   public pieChartLabels: Label[] = [];
-  public pieChartData: SingleDataSet = [25, 30, 17, 15, 16];
+  public pieChartData: SingleDataSet = [];
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
   public pieChartPlugins = [];
 
 
-  getFacultyLabel(): any {
+  getFacultyChart(): any {
     if (this.facultyList) {
       this.pieChartLabels = this.facultyList.map(faculty => {
         return ['Department of', `${faculty.facultyName}`];
       });
       console.log(this.pieChartLabels);
+      console.log(this.facultyList);
+      this.facultyList.forEach(f => {
+        this.commonService.getSubmissionCount({
+          facultyId: f.facultyId,
+        }).subscribe(response => {
+          if (response.success && response.data) {
+            this.pieChartData.push(response.data.length);
+          }
+        });
+      });
+      console.log(this.pieChartData);
     }
   }
-
-
-  /**getDaySinceSub(): any {
-
-    console.log(this.assignmentList[0]);
-    const a = new Date(this.assignmentList[0].assignment.deadline.endDate);
-    console.log(a);
-    const b = new Date();
-    console.log(b);
-    const c = Math.floor((b.getTime() - a.getTime()) / (1000 * 3600 * 24));
-    console.log(c);
-    console.log(b < a);
-  }*/
-
-
 
   ngOnInit(): void {
     this.commonService.searchAssignment({
       facultyId: null,
       username: '',
       deadlineId: null
-    }).subscribe(el => {
-      this.assignmentList = el.data;
+    }).subscribe(a => {
+      this.assignmentList = a.data;
       console.log(this.assignmentList);
+
+      this.commonService.getFaculties().subscribe(f => {
+        this.facultyList = f.data;
+        this.getFacultyChart();
+        console.log(this.facultyList);
+
+        this.commonService.searchSubmission({
+          username: '',
+          assignmentId: null,
+          status: null
+        }).subscribe(s => {
+          this.submissionList = s.data;
+          console.log(this.submissionList);
+
+          this.submissionList.forEach(s => {
+            this.assignmentList.forEach(a => {
+              if (s.assignmentId === a.assignment.assignmentId) {
+                s.facultyId = a.assignment.facultyId;
+                s.assignmentName = a.assignment.assignmentName;
+                s.deadline = a.assignment.deadline;
+                this.facultyList.forEach(f => {
+                  if (f.facultyId === s.facultyId) {
+                    s.facultyName = f.facultyName;
+                  }
+                });
+              }
+            });
+          });
+          console.log(this.submissionList);
+
+          this.submissionList.forEach(s => {
+
+            const a = new Date(s.deadline.endDate);
+            const b = new Date();
+            const c = new Date(s.deadline.startDate);
+            if (a.getTime() < b.getTime() && c.getFullYear() === b.getFullYear()) {
+              s.daysOverdue = Math.floor((b.getTime() - a.getTime()) / (1000 * 3600 * 24));
+              this.overDueSub.push(s);
+            } else if (a.getTime() > b.getTime() && c.getFullYear() === b.getFullYear()) {
+              const d = new Date(s.submissionDate);
+              s.daysSinceSubs = Math.floor((b.getTime() - d.getTime()) / (1000 * 3600 * 24));
+              this.notCommentedYet.push(s);
+            }
+          });
+
+          console.log(this.overDueSub);
+          console.log(this.notCommentedYet);
+        });
+      });
     });
 
-    this.commonService.getDeadline({
-      deadlineId: this.deadlineId
-    }).subscribe(response => {
-      console.log(response.data);
-    });
+
   }
 }
