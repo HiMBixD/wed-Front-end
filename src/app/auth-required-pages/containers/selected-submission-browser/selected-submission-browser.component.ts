563052d2677535aa8d99cd65483ff089ab6e47d4
havorx <55874008+jazzkatxlemon@users.noreply.github.com>
2021-04-14 12:01:44 +0700

    fix color dashboard, adding preview file to all except evaluation (#16)
    * Revert "Revert "done dashboard & implementing view img file""
    
    This reverts commit e850e0f71a577165586a3f7fe4f8ca68894f46ed.
    
    * fixing dashboard
    
    * Re-merge
    
    * fix dashboard color
    
    * adding preview file to new-submission, guest-portal
    
    * Create img.png
    
    Co-authored-by: Uyen Nguyen <55873366+uyen18827@users.noreply.github.com>

--- src/app/auth-required-pages/containers/selected-submission-browser/selected-submission-browser.component.ts
+++ src/app/auth-required-pages/containers/selected-submission-browser/selected-submission-browser.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { assignmentStatus } from '../../interfaces/assignment';
-import { CommonService } from '../../services/common.service';
+import {Component, OnInit} from '@angular/core';
+import {assignmentStatus} from '../../interfaces/assignment';
+import {CommonService} from '../../services/common.service';
+import {DomSanitizer} from '@angular/platform-browser';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+import {Location} from '@angular/common';
+import {environment} from '../../../../environments/environment';
 
 @Component({
   selector: 'app-selected-submission-browser',
@@ -10,8 +15,14 @@ import { CommonService } from '../../services/common.service';
 export class SelectedSubmissionBrowserComponent implements OnInit {
   userDetails: any;
   facultyList: any;
+  viewedFile: any;
+  fileTypes = ['apng', 'avif', 'gif', 'jpg', 'jpeg', 'jfif', 'pjpeg', 'pjp', 'png', 'svg', 'webp'];
+  url = `${environment.apiUrl}/file/read/`;
 
-  constructor(private commonService: CommonService) { }
+
+  constructor(private commonService: CommonService,
+              private sanitizer: DomSanitizer) {
+  }
 
   ngOnInit(): void {
     // this.commonService.getMyInfo({}).subscribe(val => {
@@ -28,7 +39,7 @@ export class SelectedSubmissionBrowserComponent implements OnInit {
           // console.log(f);
         }
       }
-    )
+    );
 
     this.commonService.searchSubmission({
       username: '',
@@ -37,30 +48,45 @@ export class SelectedSubmissionBrowserComponent implements OnInit {
     }).subscribe(value => {
       if (value) {
         this.selectedSubmissions = value.data;
-        console.log(this.selectedSubmissions)
+        console.log(this.selectedSubmissions);
       }
     });
   }
+
   //////////////////////////////////////////////////
-  p: number = 1
+  p: number = 1;
   assignmentPage: number = 1;
   submissionPage: number = 1;
   currentUser: string;
 
-  selectedSubmissions = []
+  selectedSubmissions = [];
   assignmentList = [];
   submissionList = [];
   filesList = [];
+
   ////////////////////////////////////////////////
+
+  checkFileTypes(): any {
+    const fileExtension = this.viewedFile?.fileName?.split('.').reverse()[0];
+    return this.fileTypes.includes(fileExtension?.toLowerCase());
+  }
+
+  transform(url): any {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 
   getFacultyName(facultyId) {
     let found = this.facultyList.find(faculty => +faculty.facultyId === +facultyId);
-    return found.facultyName
+    return found.facultyName;
   }
+
   //this.userDetails.facultyId
+  fileIdViewed: any;
+
   /**
-   * Show all assignments 
+   * Show all assignments
    */
+
   showAllAssignments() {
     this.commonService.searchAssignment({
       facultyId: '',
@@ -70,30 +96,31 @@ export class SelectedSubmissionBrowserComponent implements OnInit {
       list => {
         if (list) {
           this.assignmentList = list.data;
-          console.log(list)
+          console.log(list);
         }
       }
-    )
+    );
   }
 
   /**
    * Show submissions by AssignmentId
    * @param assId Assignment's Id
    */
-  showAllSubmissions(assId: any) {
+  showAllSubmissions(assId: any): any {
     this.commonService.searchSubmission({
       username: '',
       assignmentId: assId,
       status: assignmentStatus.selected
     }).subscribe(value => {
       if (value) {
-        this.submissionList = value.data
-        console.log(value)
+        this.submissionList = value.data;
+        console.log(value);
       }
-    })
+    });
   }
-  getFiles(submissionId, username) {
-    this.commonService.getFilesBySub({ submissionId }).subscribe(
+
+  getFiles(submissionId, username): any {
+    this.commonService.getFilesBySub({submissionId}).subscribe(
       value => {
         if (value.success) {
           this.filesList = value.data;
@@ -103,13 +130,12 @@ export class SelectedSubmissionBrowserComponent implements OnInit {
     );
     this.currentUser = username;
   }
-
-  viewFiles() {
-    
-  }
 }
+
 interface selectedSubmission {
   topic: string,
   username: string,
   date: string,
 }
+
+
