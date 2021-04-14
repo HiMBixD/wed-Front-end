603aad005eeaea2732b9442c3a0a23e3b2cb5bc6
Uyen Nguyen <55873366+uyen18827@users.noreply.github.com>
2021-04-14 14:48:40 +0700

    func: fix

--- src/app/auth-required-pages/containers/selected-submission-browser/selected-submission-browser.component.ts
+++ src/app/auth-required-pages/containers/selected-submission-browser/selected-submission-browser.component.ts
@@ -6,6 +6,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {ToastrService} from 'ngx-toastr';
 import {Location} from '@angular/common';
 import {environment} from '../../../../environments/environment';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-selected-submission-browser',
@@ -63,7 +64,7 @@ export class SelectedSubmissionBrowserComponent implements OnInit {
   assignmentList = [];
   submissionList = [];
   filesList = [];
-
+  deadlineSearch = new FormControl('')
   ////////////////////////////////////////////////
 
   checkFileTypes(): any {
@@ -129,6 +130,19 @@ export class SelectedSubmissionBrowserComponent implements OnInit {
       }
     );
     this.currentUser = username;
+  }
+  searchSubmissionByDeadline() {
+    this.commonService.searchAssignment(
+      {
+        facultyId: null,
+        deadlineId: parseInt(this.deadlineSearch.value),
+        username: '',
+      }
+    ).subscribe(
+      value => {
+        this.assignmentList = value.data;
+      }
+    )
   }
 }
 
